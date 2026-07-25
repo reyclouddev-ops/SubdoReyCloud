16ce926f95b4c203049d77e524f84082a817eacc
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-25 21:26:00 +0700

    Create register.js

---
+++ public/register.js
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Register</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
+
+
+<body>
+
+
+<div class="card">
+
+
+<h2>📝 Register</h2>
+
+
+<input id="username"
+placeholder="Username">
+
+
+<input id="email"
+placeholder="Email">
+
+
+<input id="password"
+type="password"
+placeholder="Password">
+
+
+<button onclick="register()">
+REGISTER
+</button>
+
+
+<p>
+Sudah punya akun?
+<a href="login.html">
+Login
+</a>
+</p>
+
+
+</div>
+
+
+<script src="js/auth.js"></script>
+
+
+</body>
+</html>
