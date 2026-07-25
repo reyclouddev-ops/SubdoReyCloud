c0300bf2e1f30e1befcbe35c24824df90ccd3245
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-25 21:45:10 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const connectDB = require("../config/db")
 
 const authRoute = require("../routes/auth")
 const dnsRoute = require("../routes/dns")
+const adminRoute = require("../routes/admin")
 
 
 const app = express()
@@ -30,6 +31,61 @@ app.get("/api", (req,res)=>{
 app.use("/api/auth", authRoute)
 
 app.use("/api/dns", dnsRoute)
+
+app.use("/api/admin",adminRoute)
+
+router.get(
+"/stats",
+auth,
+role(
+"Owner",
+"Admin"
+),
+async(req,res)=>{
+
+
+const totalUser =
+await User.countDocuments()
+
+
+const totalDNS =
+await DNS.countDocuments()
+
+
+
+const admin =
+await User.countDocuments({
+role:"Admin"
+})
+
+
+const reseller =
+await User.countDocuments({
+role:"Reseller"
+})
+
+
+
+res.json({
+
+success:true,
+
+stats:{
+
+user:totalUser,
+
+dns:totalDNS,
+
+admin,
+
+reseller
+
+}
+
+})
+
+
+})
 
 
 module.exports = app
